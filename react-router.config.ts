42b188d1f69517360de71432afe1001a898e0ecb
Abhijeet-ravan <abhijeet@ravan.ai>
2026-04-14 21:09:30 +0530

    swelling

--- react-router.config.ts
+++ react-router.config.ts
@@ -10,4 +10,7 @@ import {hydrogenPreset} from '@shopify/hydrogen/react-router-preset';
  */
 export default {
   presets: [hydrogenPreset()],
+  future: {
+    v8_middleware: false,
+  },
 } satisfies Config;
